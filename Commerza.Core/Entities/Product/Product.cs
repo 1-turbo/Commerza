140567cae1ba219f1e06a77c6941968f034fb5bc
1-turbo <aabdelsalamm1@gmail.com>
2025-06-02 22:54:13 +0300

    add base entity and product-related domain models (Product, Category, Photo)

---
+++ Commerza.Core/Entities/Product/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Commerza.Core.Entities.Product
+{
+    public class Product: BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public virtual List<Photo> Photos { get; set; }
+        public int CategoryID { get; set; }
+        [ForeignKey(nameof(CategoryID))]
+        public virtual Category Category { get; set; }
+    }
+}
