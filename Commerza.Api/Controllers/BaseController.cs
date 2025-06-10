601925bcd85eeab22f92ac2f3ea57b3bcefee657
1-turbo <aabdelsalamm1@gmail.com>
2025-06-10 18:21:05 +0300

    add Categories API controller with get-all endpoint.

---
+++ Commerza.Api/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Commerza.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commerza.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork work;
+
+        public BaseController(IUnitOfWork work)
+        {
+            this.work = work;
+        }
+    }
+}
---
+++ Commerza.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Commerza.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commerza.Api.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        public CategoriesController(IUnitOfWork work) : base(work)
+        {
+        }
+        [HttpGet("get-all")]
+        public async Task<IActionResult> get()
+        {
+            try
+            {
+                var category = await work.CategoryRepositry.GetAllAsync();
+                if (category is null)
+                    return BadRequest();
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);  
+            }
+        }
+    }
+}
